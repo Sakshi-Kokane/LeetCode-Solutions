3dfa4c7bfd660d8c2fded6633fdd21e7e7faee73
Sakshi-Kokane <kokanesakshi280@gmail.com>
2026-08-21 17:43:52 +0530

    Time: 1 ms (58.42%), Space: 47.5 MB (81.48%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -3,30 +3,36 @@ class Solution
     public int trap(int[] height) 
     {
         int n = height.length;
-        if(n==0)
-        {
-            return 0;
-        }
         int total=0;
-        int[] prefixMax= new int[n];
-        int[] suffixMax=new int[n];
-        prefixMax[0]=height[0];
-        for(int i=1; i<n; i++)
+        int leftmax=0;
+        int rightmax=0;
+        int l=0;
+        int r=n-1;
+        while(l<r)
         {
-            prefixMax[i]=Math.max(prefixMax[i-1], height[i]);
-        }
-        suffixMax[n-1]=height[n-1];
-        for(int i=n-2; i>=0; i--)
-        {
-            suffixMax[i]=Math.max(suffixMax[i+1], height[i]);
-        }
-        for(int i=0; i<n; i++)
-        {
-            int leftMax=prefixMax[i];
-            int rightMax=suffixMax[i];
-            if(height[i]<leftMax && height[i]<rightMax)
+            if(height[l]<=height[r])
             {
-                total+=Math.min(leftMax, rightMax)-height[i];
+                if(leftmax>height[l])
+                {
+                    total+=leftmax-height[l];
+                }
+                else
+                {
+                    leftmax=height[l];
+                }
+                l++;
+            }
+            else
+            {
+                if(rightmax>height[r])
+                {
+                    total+=rightmax-height[r];
+                }
+                else
+                {
+                    rightmax=height[r];
+                }
+                r--;
             }
         }
         return total;
